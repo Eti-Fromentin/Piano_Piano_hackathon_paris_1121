e13002ca395eb025466de3d86a03fd7d0f4449ef
sarah <minipouce75@gmail.com>
2021-11-25 16:56:32 +0100

    z-index for the background

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -6,7 +6,7 @@ const PianoBackground = () => {
   return (
     <div className='App'>
      <div className='NavBar'></div>
-       <video className="BackgroundVideo" autoPlay loop muted>
+       <video className="BackgroundVideo" autoPlay loop muted >
         <source src={Video} type={"video/mp4"}/> 
         </video>
       <div className="Piano"></div>
