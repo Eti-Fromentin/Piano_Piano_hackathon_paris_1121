bd5651f3ca61fdc965c6f37b07323d6ebeeb6973
sarah <minipouce75@gmail.com>
2021-11-25 16:30:58 +0100

    background video operational

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Video from '../assets/Ocean-69949.mp4'
+import './Background.css'
 
-function Background() {
+const PianoBackground = () => {
   return (
-    <div className="Video" autoPlay loop muted style={{zIndex:"-1"}}>
-      <video>
-        <source src={Video} type={"video/mp4"}/>
-      </video>
-      
+    <div className='App'>
+     <div className='NavBar'></div>
+       <video className="BackgroundVideo" autoPlay loop muted>
+        <source src={Video} type={"video/mp4"}/> 
+        </video>
+      <div className="Piano"></div>
     </div>
   )
 }
 
-export default Background
+export default PianoBackground;
