0a2b1841b82bd3013cf60e12fd2edd889d4a3f09
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-25 16:18:36 +0100

    piano key repared

--- src/piano.js
+++ src/piano.js
@@ -9,6 +9,54 @@ function PianoKeyboard() {
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
 
+  const Keyshortcuts = {
+    HOME_ROW: [{
+      natural: 'q',
+      flat: 'a',
+      sharp: 'z'
+    }, {
+      natural: 's',
+      flat: 'z',
+      sharp: 'e'
+    }, {
+      natural: 'd',
+      flat: 'e',
+      sharp: 'r'
+    }, {
+      natural: 'f',
+      flat: 'r',
+      sharp: 't'
+    }, {
+      natural: 'g',
+      flat: 't',
+      sharp: 'y'
+    }, {
+      natural: 'h',
+      flat: 'y',
+      sharp: 'u'
+    }, {
+      natural: 'j',
+      flat: 'u',
+      sharp: 'i'
+    }, {
+      natural: 'k',
+      flat: 'i',
+      sharp: 'o'
+    }, {
+      natural: 'l',
+      flat: 'o',
+      sharp: 'p'
+    }, {
+      natural: 'm',
+      flat: 'p',
+      sharp: 'ù'
+    }, {
+      natural: "!",
+      flat: ':',
+      sharp: ';'
+    }],
+  };
+
   const noteRange = {
     first: MidiNumbers.fromNote("a3"),
     last: MidiNumbers.fromNote("d5"),
@@ -16,7 +64,7 @@ function PianoKeyboard() {
   const keyboardShortcuts = KeyboardShortcuts.create({
     firstNote: noteRange.first,
     lastNote: noteRange.last,
-    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    keyboardConfig: Keyshortcuts.HOME_ROW,
   });
 
 
