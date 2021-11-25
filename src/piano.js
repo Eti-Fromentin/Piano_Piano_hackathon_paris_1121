4489979bb9b9d2523e70cd353c83ddb40c495a8e
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-25 16:05:54 +0100

    piano

--- src/piano.js
+++ src/piano.js
@@ -1,31 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import "react-piano/dist/styles.css";
 
-import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
-import 'react-piano/dist/styles.css';
+import SoundfontProvider from "./SoundfontProvider";
+import "./piano.css";
 
 function PianoKeyboard() {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
 
-    const firstNote = MidiNumbers.fromNote('d2');
-    const lastNote = MidiNumbers.fromNote('e6');
-    const keyboardShortcuts = KeyboardShortcuts.create({
-      firstNote: firstNote,
-      lastNote: lastNote,
-      keyboardConfig: KeyboardShortcuts.HOME_ROW,
-    });
+  const noteRange = {
+    first: MidiNumbers.fromNote("a3"),
+    last: MidiNumbers.fromNote("d5"),
+  };
+  const keyboardShortcuts = KeyboardShortcuts.create({
+    firstNote: noteRange.first,
+    lastNote: noteRange.last,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+  });
+
+
     return (
-        <Piano
-      noteRange={{ first: firstNote, last: lastNote }}
-      playNote={(midiNumber) => {
-        // Play a given note - see notes below
-      }}
-      stopNote={(midiNumber) => {
-        // Stop playing a given note - see notes below
-      }}
-      width={1000}
-      keyboardShortcuts={keyboardShortcuts}
-    />
-    )
+      <SoundfontProvider
+        instrumentName="acoustic_grand_piano"
+        audioContext={audioContext}
+        hostname={soundfontHostname}
+        render={({ isLoading, playNote, stopNote }) => (
+          <Piano
+            noteRange={noteRange}
+            width={1500}
+            playNote={playNote}
+            stopNote={stopNote}
+            disabled={isLoading}
+            keyboardShortcuts={keyboardShortcuts}
+          />
+        )}
+      />
+    
+  
+
+
+  );
 }
 
 export default PianoKeyboard;
-
