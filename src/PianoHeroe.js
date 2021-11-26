a5565e8597c790a017bc99e6bca5c0d417bb2b15
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-26 06:42:36 +0100

    Fixed button & css

--- src/PianoHeroe.js
+++ src/PianoHeroe.js
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
- import "swiper/swiper-bundle.min.css";
+import "swiper/swiper-bundle.min.css";
 
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./PianoHeroe.css";
 
 import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
-
-import "./PianoHeroe.css";
 
 function PianoHeroe() {
   const lettresDeHélise = ["G", "T", "G", "T", "G", "S", "F", "D", "Q"];
@@ -20,67 +15,39 @@ function PianoHeroe() {
 
   return (
     <section class="damier">
-      <button
+      <button class="button"
         onClick={() => {
           setDisplayCard(!displayCard);
         }}
       >
-        {" "}
-        Lettre à Hélise{" "}
+        Lettre à Elise
       </button>
-      <div class="lineA">
-        <>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            className="mySwiper"
-          >
-            {displayCard &&
-              lettresDeHélise.map((element, index) => {
-                return (
-                  <SwiperSlide key={index}>
-                    <div className="case" key={index}>
-                      {" "}
-                      {element}{" "}
-                    </div>
-                  </SwiperSlide>
-                );
-              })}
-          </Swiper>
-        </>
-        )
-        {/* {displayCard && lettresDeHélise.map((element, index) => {
-        return (
-        <div className="case" key={index}> {element} </div>
-        )
-    })} */}
-        {/* <div id="signeQ" class="case">Case</div> */}
-        {/* <div id="signeZ" class="case">Case Z</div>
-   <div id="signeR" class="case">Case S</div> */}
-        {/* <div class="signeD" id="case">Case D</div>
-   <div class="signeR" id="case">Case R</div>
-   <div class="signeF" id="case">Case F</div>
-   <div class="signeT" id="case">Case T</div>
-   <div class="signeG" id="case">Case G</div>
-   <div class="signeH" id="case">Case H</div>
-   <div class="signeU" id="case">Case U</div>
-   <div class="signeJ" id="case">Case J</div>
-   <div class="signeI" id="case">Case I</div>
-   <div class="signeK" id="case">Case K</div>
-   <div class="signeO" id="case">Case O</div>
-   <div class="signeL" id="case">Case L</div>
-   <div class="signeM" id="case">Case M</div>
-   <div class="signeP" id="case">Case P</div>
-   <div class="signeù" id="case">Case ù</div> */}
-      </div>
+      {displayCard &&   <div class="lineA">
+      <Swiper
+          spaceBetween={1}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={false}
+          className="mySwiper"
+        >
+          
+            {lettresDeHélise.map((element, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className="case" key={index}>
+                   <p> {element} </p>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>}
     </section>
   );
 }
