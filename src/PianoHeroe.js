59712bb15e987b8ceb3d6e0835fd2cc8e94bda84
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2021-11-26 02:14:07 +0100

    Working on JS PianoHeroe

--- src/PianoHeroe.js
+++ src/PianoHeroe.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PianoHeroe.css'
 
 function PianoHeroe() {
+    // const lettresDeHélise = ["G", "T", "G", "T", "G", "S", "F", "D", "Q"]
+    // const [displayCards, setDisplayCards] = useState('none')
+    // let timer = setInterval( showDiv, 5000);
+    // let counter = 0;
+    // let div = 
+    
+    // function showDiv(div) {
+    //       div.display: none;
+    //     }
+
 
 return (
     <section class='damier'>
         <div class="lineA">
-   <div class="signeQ" id="case">Case Q</div>
-   <div class="signeZ" id="case">Case Z</div>
-   <div class="signeS" id="case">Case S</div>
-   <div class="signeD" id="case">Case D</div>
+   <div id="signeQ" class="case">Case Q</div>
+   <div id="signeZ" class="case">Case Z</div>
+   <div id="signeR" class="case">Case S</div>
+   {/* <div class="signeD" id="case">Case D</div>
    <div class="signeR" id="case">Case R</div>
    <div class="signeF" id="case">Case F</div>
    <div class="signeT" id="case">Case T</div>
@@ -23,7 +33,7 @@ return (
    <div class="signeL" id="case">Case L</div>
    <div class="signeM" id="case">Case M</div>
    <div class="signeP" id="case">Case P</div>
-   <div class="signeù" id="case">Case ù</div>
+   <div class="signeù" id="case">Case ù</div> */}
    
    </div>
    </section>
